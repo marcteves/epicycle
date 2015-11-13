d196f89098aa921cdf14bc8f2c75c97a875231f5
Marc T <gmarc.teves@gmail.com>
2015-11-13 13:29:27 +0800

    value passed to orbiter in changeProperty () is now cast to a Number

--- scripts.js
+++ scripts.js
@@ -284,7 +284,7 @@ function changeProperty(select, property){
 	 }
 	 
 	 if (pass){
-	  orbiters[select][property] = input.value; 
+	  orbiters[select][property] = Number(input.value); 
 	  input.previousSibling.nodeValue = input.value;
 	 }
 }
